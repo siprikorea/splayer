7202c9978316e4c04e3279491ec8b1218558f631
chanbong.park <siprikorea!@gmail.com>
2016-11-07 00:05:11 +0900

    chanbong.park   - splayer: modified to use zplay

--- splayer/splayerdlg.cpp
+++ splayer/splayerdlg.cpp
@@ -6,22 +6,18 @@
 #include "splayer.h"
 #include "splayerDlg.h"
 #include "afxdialogex.h"
+#include "../libzplay/libzplay.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
 
 
-CSplayerDlg::CSplayerDlg(CWnd* pParent /*=NULL*/)
-	: CDialogEx(IDD_SPLAYER_DIALOG, pParent)
-{
-	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
-}
+using namespace libZPlay;
 
-void CSplayerDlg::DoDataExchange(CDataExchange* pDX)
-{
-	CDialogEx::DoDataExchange(pDX);
-}
+
+ZPlay* g_ZPlay = NULL;
+
 
 BEGIN_MESSAGE_MAP(CSplayerDlg, CDialogEx)
 	ON_WM_PAINT()
@@ -29,35 +25,44 @@ BEGIN_MESSAGE_MAP(CSplayerDlg, CDialogEx)
 END_MESSAGE_MAP()
 
 
-// CSplayerDlg 메시지 처리기
+CSplayerDlg::CSplayerDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(IDD_SPLAYER_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+
+	g_ZPlay = CreateZPlay();
+}
+
+CSplayerDlg::~CSplayerDlg()
+{
+	g_ZPlay->Release();
+}
+
+void CSplayerDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
 
 BOOL CSplayerDlg::OnInitDialog()
 {
 	CDialogEx::OnInitDialog();
 
-	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
-	//  프레임워크가 이 작업을 자동으로 수행합니다.
-	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
-	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+	SetIcon(m_hIcon, TRUE);
+	SetIcon(m_hIcon, FALSE);
 
-	// TODO: 여기에 추가 초기화 작업을 추가합니다.
 
-	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+	return TRUE;
 }
-
-// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
-//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 응용 프로그램의 경우에는
-//  프레임워크에서 이 작업을 자동으로 수행합니다.
 
 void CSplayerDlg::OnPaint()
 {
 	if (IsIconic())
 	{
-		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+		CPaintDC dc(this);
 
 		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
 
-		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
 		int cxIcon = GetSystemMetrics(SM_CXICON);
 		int cyIcon = GetSystemMetrics(SM_CYICON);
 		CRect rect;
@@ -65,7 +70,6 @@ void CSplayerDlg::OnPaint()
 		int x = (rect.Width() - cxIcon + 1) / 2;
 		int y = (rect.Height() - cyIcon + 1) / 2;
 
-		// 아이콘을 그립니다.
 		dc.DrawIcon(x, y, m_hIcon);
 	}
 	else
@@ -74,10 +78,7 @@ void CSplayerDlg::OnPaint()
 	}
 }
 
-// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
-//  이 함수를 호출합니다.
 HCURSOR CSplayerDlg::OnQueryDragIcon()
 {
 	return static_cast<HCURSOR>(m_hIcon);
 }
-
